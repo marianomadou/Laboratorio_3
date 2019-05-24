fce636d19e4942339641f68d99126db1f68609a3
marianomadou <marianomadou@gmail.com>
2019-05-24 13:02:42 -0300

    boton de cargarPersonas deshabilitado una vez que carga el JSON

--- Clase_05_bauss/js/script.js
+++ Clase_05_bauss/js/script.js
@@ -1,30 +1,55 @@
-
 var boton;
-addEventListener('load', function(){
 
-    boton=document.getElementById('btnRun');
-    boton.addEventListener('click', ejecutar, false);
+
+addEventListener('load', function(){
+    boton=document.getElementById('btnCargar');
+    boton.addEventListener('click', CargarTabla, false);
 
 }, false);
 
-function ejecutar(){
-    //alert('Hola mundo');
-    var lista = document.getElementById('lista');
+function CargarTabla(){
 
-    //console.log(lista.childNodes.length);
-    //console.log(lista.childNodes);
-    console.log(lista.children);
-   var hijos=lista.childNodes;
-    for (var index = 0; index < hijos.length; index++) {
-        
-        if(hijos[index].nodeType == Node.ELEMENT_NODE)
-        {
-            console.log(hijos[index]);
-        }
-        
+    let div = document.getElementById('infoDiv');
+    let tabla = document.createElement('table');
+    let header = document.createElement('tr');
+	
+	tabla.setAttribute('width', '500px');
+    tabla.setAttribute('style', 'border-collapse: collapse');
+	tabla.setAttribute('border', '1px solid red');
+
+    for (let key in personas[0]) {
+        let th = document.createElement('th');
+        let texto = document.createTextNode(key);
+        th.appendChild(texto);
+        header.appendChild(th);
     }
+
+    tabla.appendChild(header);
+
+    for (let i in personas) {
+        let fila = document.createElement('tr');
+        
+        for (let j in personas[i]) {
+            let celda = document.createElement('td');
+            celda.setAttribute('style', 'text-align: center');
+            let dato = document.createTextNode(personas[i][j]);
+            
+            celda.appendChild(dato);
+            fila.appendChild(celda);
+        }
+
+        tabla.appendChild(fila);
+    }
+
+    div.appendChild(tabla);
+
+//    let body = document.getElementsByTagName('body');
+    //let btnTabla = document.getElementById("btnTabla");
+//   body[0].removeChild(btnTabla);
 
 }
 
-
+function deshabilitarBoton(btn){
+		document.getElementById(btn.id).disabled = true;
+}
 
